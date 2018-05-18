6d30a69c6899b904d4a41b1535c8a6f7dfd49a9b
mizchi <miz404@gmail.com>
2018-05-19 01:23:42 +0900

    Add build script

--- App.js
+++ App.js
@@ -1,17 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+/* @flow */
 
 import React, { Component } from 'react'
 import { Platform, StyleSheet, Text, View } from 'react-native'
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
+  dom: 'Press Cmd+R to reload,\n'
 })
 
 type Props = {}
@@ -19,7 +12,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!2</Text>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
